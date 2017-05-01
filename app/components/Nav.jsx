99e94581822258eec0f25161a903daad0feaa41f
MattWhittle <themwproject@yahoo.com>
2017-05-01 09:28:24 -0400

    Initial Commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = React.createClass({
+    render: function() {
+        return(
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">React Time App</li>
+                        <li><IndexLink to="/">Timer</IndexLink></li>
+                        <li><Link to="/">Countdown</Link></li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <ul className="menu">
+                        <li className="menu-text">Created by:</li>
+                        <li><a href="https://github.com/MattWhittle" target="_blank">Matt Whittle</a></li>
+                    </ul>
+                </div>
+            </div>
+        ); 
+    }
+});
+
+module.exports = Nav;
