8281c47c81bb2899f5c963e4b8ccbd83cda02bb8
Matthew Whittle <themwproject@yahoo.com>
2017-05-04 09:49:27 -0400

    Fixing errors from pulling project

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var React = require('react');
 var Clock = require('Clock');
 
@@ -12,30 +11,4 @@ var Countdown = React.createClass({
     }
 });
 
-=======
-var React = require('react');
-var Clock = require('Clock');
-var CountdownForm = require('CountdownForm');
-
-var Countdown = React.createClass({
-    getInitialState: function(){
-        return {count: 0};
-    },
-    handleSetCountdown: function(seconds) {
-        this.setState({
-            count: seconds
-        })
-    },
-    render: function() {
-        var {count} = this.state;
-        return(
-            <div>
-                <Clock totalSeconds={count} />
-                <CountdownForm onSetCountdown={this.handleSetCountdown} />
-            </div>
-        );
-    }
-});
-
->>>>>>> 7102544a61d94b6b21b22d9e9c5548cd0d0f48ed
 module.exports = Countdown;
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var React = require('react');
 var Nav = require('Nav');
 
@@ -17,25 +16,4 @@ var Main = (props) => {
         </div> 
     )
 };
-
-=======
-var React = require('react');
-var Nav = require('Nav');
-
-
-
-var Main = (props) => {
-    return (
-        <div>
-          <Nav/>
-            <div className="row">
-                <div className="column small-centered medium-6 larger-4">
-                    {props.children}
-                </div>
-            </div>
-        </div> 
-    )
-};
-
->>>>>>> 7102544a61d94b6b21b22d9e9c5548cd0d0f48ed
 module.exports = Main;
